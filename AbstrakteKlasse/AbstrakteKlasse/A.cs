21b8bb8da4f5269a3f2ce501e6d2fe51a8df9f28
Ibra1101 <116690598+Ibra1101@users.noreply.github.com>
2023-03-14 17:12:05 +0100

    Add files via upload

---
+++ AbstrakteKlasse/AbstrakteKlasse/A.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstrakteKlasse
+{
+    public abstract class A
+    {
+        public abstract void DoWork(int i);
+        public abstract int RueckGabe();
+    }
+}
---
+++ AbstrakteKlasse/AbstrakteKlasse/B.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstrakteKlasse
+{
+    public  class B : A
+    {
+        public override void DoWork(int i)
+        {
+
+        }
+        public override int RueckGabe()
+        {
+            return 5;
+        }
+
+    }
+}
