102290b6aec59b17a50258c1a1c9730985f60feb
Ibra1101 <116690598+Ibra1101@users.noreply.github.com>
2023-03-14 19:38:16 +0100

    Add files via upload

---
+++ Zahlen_durch_funf/Zahlen_durch_funf/Program.cs
@@ -0,0 +1,12 @@
+﻿class Teiler5 
+{
+    static double[] zahlen = { 3.6, 5.4, 8.3, 1.93, 7.2, 12.55 };
+    static void Main()
+    {
+        foreach(double i in zahlen)
+        {
+           Console.WriteLine(Math.Round(i/5)*5);
+        }
+    }
+
+}
